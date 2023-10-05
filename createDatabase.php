c7de5006242e45565a27a25ed9217d29841e5f50
MatejMijok <78339573+MatejMijok@users.noreply.github.com>
2023-10-05 14:39:42 +0200

    Update createDatabase.php

--- createDatabase.php
+++ createDatabase.php
@@ -24,7 +24,7 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 
 $sql = "CREATE TABLE IF NOT EXISTS LANGUAGE(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    lang VARCHAR(100)
+    lang VARCHAR(100) UNIQUE
 )";
 
 if(mysqli_query($conn, $sql)){
@@ -35,13 +35,13 @@ else{
 }
 
 
-
 $sql = "CREATE TABLE IF NOT EXISTS MEAL(
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(100),
     description VARCHAR(100),
     status VARCHAR(10) DEFAULT 'CREATED',
     id_language INT,
+    id_category INT,
     FOREIGN KEY (id_language) REFERENCES LANGUAGE(id)
 )";
 
@@ -58,9 +58,9 @@ $sql = "CREATE TABLE IF NOT EXISTS TAG(
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(100),
     slug VARCHAR(100),
-    meal_id INT,
+    id_meal INT,
     id_language INT,
-    FOREIGN KEY (meal_id) REFERENCES MEAL(id),
+    FOREIGN KEY (id_meal) REFERENCES MEAL(id),
     FOREIGN KEY (id_language) REFERENCES LANGUAGE(id)
 )";
 
@@ -78,8 +78,8 @@ $sql = "CREATE TABLE IF NOT EXISTS INGREDIENT(
     title VARCHAR(100),
     slug VARCHAR(100),
     id_language INT,
-    meal_id int,
-    FOREIGN KEY (meal_id) REFERENCES MEAL(id),
+    id_meal int,
+    FOREIGN KEY (id_meal) REFERENCES MEAL(id),
     FOREIGN KEY (id_language) REFERENCES LANGUAGE(id)
 )";
 
@@ -97,8 +97,8 @@ $sql = "CREATE TABLE IF NOT EXISTS CATEGORY(
         title VARCHAR(100),
         slug VARCHAR(100),
         id_language INT,
-        meal_id int,
-        FOREIGN KEY (meal_id) REFERENCES MEAL(id),
+        id_meal int,
+        FOREIGN KEY (id_meal) REFERENCES MEAL(id),
         FOREIGN KEY (id_language) REFERENCES LANGUAGE(id)
 )";
 
@@ -113,11 +113,11 @@ else{
 
 $sql = "CREATE TABLE IF NOT EXISTS MEALTRANSLATION(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    meal_id INT,
-    lang_id INT,
+    id_meal INT,
+    id_language INT,
     translation VARCHAR(100),
-    FOREIGN KEY (meal_id) REFERENCES MEAL(id),
-    FOREIGN KEY (lang_id) REFERENCES LANGUAGE(id)
+    FOREIGN KEY (id_meal) REFERENCES MEAL(id),
+    FOREIGN KEY (id_language) REFERENCES LANGUAGE(id)
 )";
 
 if(mysqli_query($conn, $sql)){
@@ -131,11 +131,11 @@ else{
 
 $sql = "CREATE TABLE IF NOT EXISTS INGREDIENTTRANSLATION(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    ingredient_id INT,
-    lang_id INT,
+    id_ingredient INT,
+    id_language INT,
     translation VARCHAR(100),
-    FOREIGN KEY (ingredient_id) REFERENCES INGREDIENT(id),
-    FOREIGN KEY (lang_id) REFERENCES LANGUAGE(id)
+    FOREIGN KEY (id_ingredient) REFERENCES INGREDIENT(id),
+    FOREIGN KEY (id_language) REFERENCES LANGUAGE(id)
 )";
 
 if(mysqli_query($conn, $sql)){
@@ -150,10 +150,10 @@ else{
 $sql = "CREATE TABLE IF NOT EXISTS TAGTRANSLATION(
     id INT PRIMARY KEY AUTO_INCREMENT,
     tag_id INT,
-    lang_id INT,
+    id_language INT,
     translation VARCHAR(100),
     FOREIGN KEY (tag_id) REFERENCES TAG(id),
-    FOREIGN KEY (lang_id) REFERENCES LANGUAGE(id)
+    FOREIGN KEY (id_language) REFERENCES LANGUAGE(id)
 )";
 
 if(mysqli_query($conn, $sql)){
@@ -167,10 +167,10 @@ else{
 $sql = "CREATE TABLE IF NOT EXISTS CATEGORYTRANSLATION(
     id INT PRIMARY KEY AUTO_INCREMENT,
     category_id INT,
-    lang_id INT,
+    id_language INT,
     translation VARCHAR(100),
     FOREIGN KEY (category_id) REFERENCES CATEGORY(id),
-    FOREIGN KEY (lang_id) REFERENCES LANGUAGE(id)
+    FOREIGN KEY (id_language) REFERENCES LANGUAGE(id)
 )";
 
 if(mysqli_query($conn, $sql)){
@@ -183,11 +183,11 @@ else{
 
 $sql = "CREATE TABLE IF NOT EXISTS MEALTRANSLATION(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    meal_id INT,
-    lang_id INT,
+    id_meal INT,
+    id_language INT,
     translation VARCHAR(100),
-    FOREIGN KEY (meal_id) REFERENCES MEAL(id),
-    FOREIGN KEY (lang_id) REFERENCES LANGUAGE(id)
+    FOREIGN KEY (id_meal) REFERENCES MEAL(id),
+    FOREIGN KEY (id_language) REFERENCES LANGUAGE(id)
 )";
 
 if(mysqli_query($conn, $sql)){
